f9508d50b231fea677bc1a6732254dc19bf3bfe4
Proton <feisuzhu@163.com>
2017-06-05 15:24:37 +0800

    Update riemann to 0.2.13

--- satori/images/riemann/project.clj
+++ satori/images/riemann/project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                  [org.clojure/tools.reader "1.0.0-beta3"]
-                  [com.taoensso/carmine "2.14.0"]
-                  [riemann "0.2.11"]])
+                  [org.clojure/tools.reader "1.0.0-RC1"]
+                  [com.taoensso/carmine "2.16.0"]
+                  [riemann "0.2.13"]])
