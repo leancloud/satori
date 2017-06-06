8eda7c0aeda9bc617334582f8307258acf09bea9
Proton <feisuzhu@163.com>
2017-06-06 15:29:18 +0800

    Fix riemann build (missing clojure.data.json)

--- satori/images/riemann/project.clj
+++ satori/images/riemann/project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                  [org.clojure/data.json "0.2.6"]
                   [org.clojure/tools.reader "1.0.0-RC1"]
                   [com.taoensso/carmine "2.16.0"]
                   [riemann "0.2.13"]])
