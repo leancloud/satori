35e19a407a39fc3ec41d1a2c2b1912bdbae564a0
Proton <feisuzhu@163.com>
2018-07-19 02:33:05 +0800

    :bookmark: It's 2018

--- doc/conf.py
+++ doc/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Satori'
-copyright = u'2017, LeanCloud'
+copyright = u'2018, LeanCloud'
 author = u'LeanCloud'
 
 # The version info for the project you're documenting, acts as replacement for
