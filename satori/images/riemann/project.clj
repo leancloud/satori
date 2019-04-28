a18c2be798b3272b23a1cb7a499b08ca2bccb6fc
Proton <feisuzhu@163.com>
2019-04-28 14:17:14 +0800

    :arrow_up: Add core.match to riemann jar

--- satori/images/riemann/project.clj
+++ satori/images/riemann/project.clj
@@ -1,4 +1,4 @@
-(defproject riemann "0.3.1-satori"
+(defproject riemann "0.3.2-satori"
   :description "Satori flavored Riemann"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -7,6 +7,7 @@
   :middleware [io.aviso.lein-pretty/inject]
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.json "0.2.6"]
+                 [org.clojure/core.match "0.3.0"]
                  [com.taoensso/carmine "2.19.1"]
                  [io.aviso/pretty "0.1.37"]
                  [riemann "0.3.1"]])
