77da360e7be805871fd0718adf9b78ea6c2e9ee2
Proton <feisuzhu@163.com>
2019-05-22 19:02:41 +0800

    :arrow_up: Update riemann

--- satori/images/riemann/project.clj
+++ satori/images/riemann/project.clj
@@ -1,4 +1,4 @@
-(defproject riemann "0.3.2-satori"
+(defproject riemann "0.3.3-satori"
   :description "Satori flavored Riemann"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -8,6 +8,7 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/core.match "0.3.0"]
+                 [org.clojure/core.async "0.4.490"]
                  [com.taoensso/carmine "2.19.1"]
                  [io.aviso/pretty "0.1.37"]
-                 [riemann "0.3.1"]])
+                 [riemann "0.3.2"]])
